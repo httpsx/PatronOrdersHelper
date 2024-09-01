91a30a069c5b5691906486ecf151f57177718456
Https <httpsx1@gmail.com>
2024-09-01 18:06:10 +0500

    Fix bugs with fake rows

--- core.lua
+++ core.lua
@@ -3,8 +3,12 @@ if not POHSaved.orders then
     POHSaved.orders = {}
 end
 
-local function generateUniqueID(expirationTime, itemID, orderID)
-    local data = tostring(expirationTime) .. tostring(itemID) .. tostring(orderID)
+local sessionOrders = {}
+
+
+local function generateUniqueID(order)
+    local data = tostring(order.expirationTime) .. tostring(order.itemID) .. tostring(order.orderID)
+    --local data = tostring(order.orderID)
     local hash = 0
     for i = 1, #data do
         local char = string.byte(data, i)
@@ -24,7 +28,7 @@ local function updateOrderAlpha()
     local dataProvider = ProfessionsFrame.OrdersPage.BrowseFrame.OrderList.ScrollBox:GetDataProvider()
     local collection = dataProvider:GetCollection()
     for i = 1, #collection do
-        local uniqID = (generateUniqueID(collection[i].option.expirationTime, collection[i].option.itemID, collection[i].option.orderID))
+        local uniqID = (generateUniqueID(collection[i].option))
         if (POHSaved.orders[uniqID]) then
             setAlphaRow(_G["PublicOrdersCustomHideColumn" .. uniqID], 0.2)
         else
@@ -39,7 +43,7 @@ local function updateOrderList()
     local function swapOrders()
         local collection = dataProvider:GetCollection()
         for i = 1, #collection do
-            local uniqID = (generateUniqueID(collection[i].option.expirationTime, collection[i].option.itemID, collection[i].option.orderID))
+            local uniqID = (generateUniqueID(collection[i].option))
             if (POHSaved.orders[uniqID]) then
                 if not dbswaps[uniqID] then
                     dbswaps[uniqID] = true
@@ -58,55 +62,67 @@ local function updateOrderList()
 end
 
 
-local sessionOrders = {}
+local function resetOrderList()
+    for k, v in pairs(sessionOrders) do
+        _G["PublicOrdersCustomHideColumn" .. k]:Hide()
+        setAlphaRow(_G["PublicOrdersCustomHideColumn" .. k], 1.0)
+    end
+end
+
 EventUtil.ContinueOnAddOnLoaded("Blizzard_Professions", function()
     function ProfessionsCrafterTableCellCustomHideMixin:Populate(rowData, dataIndex)
+        local isDebug = false
         local order = rowData.option
-        local uniqID = (generateUniqueID(order.expirationTime, order.itemID, order.orderID))
+        local uniqID = (generateUniqueID(order))
+        if (order.orderType == 3) then
+            ProfessionsTableCellTextMixin.SetText(self, isDebug and uniqID or "")
+            local _, e = self.Text:GetPoint()
+            if (not _G["PublicOrdersCustomHideColumn" .. uniqID]) then
+                local checkBox = CreateFrame("CheckButton", "PublicOrdersCustomHideColumn" .. uniqID, e,
+                    "ChatConfigCheckButtonTemplate")
+                checkBox:SetChecked(POHSaved.orders[uniqID])
+                checkBox:SetPoint("CENTER", e, "CENTER", -10, 0)
+                checkBox:SetScript("OnClick", function()
+                    POHSaved.orders[uniqID] = checkBox:GetChecked()
+                    updateOrderList()
+                end)
+                sessionOrders[uniqID] = true
 
-        ProfessionsTableCellTextMixin.SetText(self, "")
-
-        local _, e = self.Text:GetPoint()
-        if (not _G["PublicOrdersCustomHideColumn" .. uniqID]) then
-            local checkBox = CreateFrame("CheckButton", "PublicOrdersCustomHideColumn" .. uniqID, e,
-                "ChatConfigCheckButtonTemplate")
-            checkBox:SetChecked(POHSaved.orders[uniqID])
-            checkBox:SetPoint("CENTER", e, "CENTER", -10, 0)
-            checkBox:SetScript("OnClick", function()
-                POHSaved.orders[uniqID] = checkBox:GetChecked()
-                updateOrderList()
-            end)
-            sessionOrders[uniqID] = true
-
-            local isDebug = false
-            if isDebug then
-                checkBox:SetScript("OnEnter", function()
-                    GameTooltip:SetOwner(checkBox, "ANCHOR_RIGHT")
-                    GameTooltip:AddLine("OfferCID: " .. uniqID)
-                    GameTooltip:Show()
+                checkBox:SetScript("OnEnter", function(self)
+                    self:GetParent():GetParent():OnEnter()
+                    if isDebug then
+                        GameTooltip:SetOwner(checkBox, "ANCHOR_RIGHT")
+                        GameTooltip:AddLine("OfferCID: " .. uniqID)
+                        GameTooltip:Show()
+                    end
                 end)
 
-                checkBox:SetScript("OnLeave", function()
-                    GameTooltip:Hide()
+                checkBox:SetScript("OnLeave", function(self)
+                    self:GetParent():GetParent():OnLeave()
+                    if isDebug then
+                        GameTooltip:Hide()
+                    end
                 end)
+            else
+                _G["PublicOrdersCustomHideColumn" .. uniqID]:Show()
+                _G["PublicOrdersCustomHideColumn" .. uniqID]:SetParent(e)
+                _G["PublicOrdersCustomHideColumn" .. uniqID]:SetPoint("CENTER", e, "CENTER", -10, 0)
             end
         else
-            _G["PublicOrdersCustomHideColumn" .. uniqID]:Show()
-            _G["PublicOrdersCustomHideColumn" .. uniqID]:SetParent(e)
-            _G["PublicOrdersCustomHideColumn" .. uniqID]:SetPoint("CENTER", e, "CENTER", -10, 0)
+            if (_G["PublicOrdersCustomHideColumn" .. uniqID]) then
+                _G["PublicOrdersCustomHideColumn" .. uniqID]:Hide()
+                _G["PublicOrdersCustomHideColumn" .. uniqID]:SetParent(nil)
+            end
         end
     end
 
-    ProfessionsFrame.OrdersPage:HookScript("OnHide", function(event)
-        for k, v in pairs(sessionOrders) do
-            _G["PublicOrdersCustomHideColumn" .. k]:Hide()
-            setAlphaRow(_G["PublicOrdersCustomHideColumn" .. k], 1.0)
-        end
+    ProfessionsFrame.OrdersPage:HookScript("OnHide", function()
+        resetOrderList()
     end)
 
     hooksecurefunc(ProfessionsFrame.OrdersPage, "SetupTable", function(self)
-        local PTC = ProfessionsTableConstants;
         if self.orderType == 3 then
+            local PTC = ProfessionsTableConstants;
             local column = self.tableBuilder:AddFixedWidthColumn(self, PTC.NoPadding, 60, PTC.Tip.LeftCellPadding,
                 PTC.Tip.RightCellPadding, nil, "ProfessionsCrafterTableCellCustomHideTemplate")
             column:ConstructHeader("BUTTON", "ProfessionsCrafterTableHeaderStringTemplate", self, "Hide")
@@ -114,7 +130,11 @@ EventUtil.ContinueOnAddOnLoaded("Blizzard_Professions", function()
         end
     end)
 
-    hooksecurefunc(ProfessionsFrame.OrdersPage, "ShowGeneric", function()
-        updateOrderList()
+    hooksecurefunc(ProfessionsFrame.OrdersPage, "ShowGeneric", function(self)
+        if self.orderType == 3 then
+            updateOrderList()
+        else
+            resetOrderList()
+        end
     end)
 end)
