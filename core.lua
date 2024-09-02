e9296038b20334826767a5a5cc67fe33a6c04109
Https <httpsx1@gmail.com>
2024-09-03 03:07:41 +0500

    v1.1 - Fix Blizzard error bug

--- core.lua
+++ core.lua
@@ -39,6 +39,7 @@ end
 local function updateOrderList()
     local dataProvider = ProfessionsFrame.OrdersPage.BrowseFrame.OrderList.ScrollBox:GetDataProvider()
     local dbswaps = {}
+
     local function swapOrders()
         local collection = dataProvider:GetCollection()
         for i = 1, #collection do
@@ -56,7 +57,7 @@ local function updateOrderList()
         end
     end
 
-    swapOrders()
+    --swapOrders()
     updateOrderAlpha()
 end
 
@@ -69,18 +70,46 @@ local function resetOrderList()
 end
 
 EventUtil.ContinueOnAddOnLoaded("Blizzard_Professions", function()
-    function ProfessionsCrafterTableCellCustomHideMixin:Populate(rowData, dataIndex)
+    --hooksecurefunc(ProfessionsCrafterTableCellCommissionMixin, "Populate", function(self, rowData)
+    --    local order = rowData.option;
+    --    local hasRewards = order.npcOrderRewards and #order.npcOrderRewards > 0;
+    --
+    --    if hasRewards then
+    --        for idx, reward in ipairs(order.npcOrderRewards) do
+    --        	local _,_,_,_,_,_,_,_,_, itemTexture = C_Item.GetItemInfo(reward.itemLink)
+    --            local texture = self:CreateTexture(nil, "BACKGROUND")
+    --            texture:SetTexture(itemTexture)
+    --            texture:SetSize(16, 16)
+    --            texture:SetPoint("LEFT", self, "LEFT", -20 + ((idx-1) * 20), 0)
+    --		end
+    --
+    --      end
+    --  end)
+
+    hooksecurefunc(ProfessionsCrafterTableCellExpirationMixin, "Populate", function(self, rowData)
         local isDebug = false
         local order = rowData.option
         local uniqID = (generateUniqueID(order))
         if (order.orderType == 3) then
-            ProfessionsTableCellTextMixin.SetText(self, isDebug and uniqID or "")
+            local remainingTime = Professions.GetCraftingOrderRemainingTime(order.expirationTime);
+            local seconds = remainingTime >= 60 and remainingTime or 60; -- Never show < 1min
+            local timeText = Professions.OrderTimeLeftFormatter:Format(seconds);
+            if seconds <= Constants.ProfessionConsts.PUBLIC_CRAFTING_ORDER_STALE_THRESHOLD then
+                timeText = ERROR_COLOR:WrapTextInColorCode(timeText);
+            end
+
+            ProfessionsTableCellTextMixin.SetText(self, isDebug and uniqID or timeText)
+            self.Text:SetJustifyH("LEFT")
+            self.Text:ClearAllPoints()
+            self.Text:SetPoint("TOPLEFT", 0, 0)
+            self.Text:SetPoint("BOTTOMRIGHT", -33, 0)
+
             local _, e = self.Text:GetPoint()
             if (not _G["PublicOrdersCustomHideColumn" .. uniqID]) then
                 local checkBox = CreateFrame("CheckButton", "PublicOrdersCustomHideColumn" .. uniqID, e,
                     "ChatConfigCheckButtonTemplate")
                 checkBox:SetChecked(POHSaved.orders[uniqID])
-                checkBox:SetPoint("CENTER", e, "CENTER", -10, 0)
+                checkBox:SetPoint("CENTER", e, "CENTER", 5, 0)
                 checkBox:SetScript("OnClick", function()
                     POHSaved.orders[uniqID] = checkBox:GetChecked()
                     updateOrderList()
@@ -105,7 +134,7 @@ EventUtil.ContinueOnAddOnLoaded("Blizzard_Professions", function()
             else
                 _G["PublicOrdersCustomHideColumn" .. uniqID]:Show()
                 _G["PublicOrdersCustomHideColumn" .. uniqID]:SetParent(e)
-                _G["PublicOrdersCustomHideColumn" .. uniqID]:SetPoint("CENTER", e, "CENTER", -10, 0)
+                _G["PublicOrdersCustomHideColumn" .. uniqID]:SetPoint("CENTER", e, "CENTER", 5, 0)
             end
         else
             if (_G["PublicOrdersCustomHideColumn" .. uniqID]) then
@@ -113,7 +142,7 @@ EventUtil.ContinueOnAddOnLoaded("Blizzard_Professions", function()
                 _G["PublicOrdersCustomHideColumn" .. uniqID]:SetParent(nil)
             end
         end
-    end
+    end)
 
     ProfessionsFrame.OrdersPage:HookScript("OnHide", function()
         resetOrderList()
@@ -121,16 +150,6 @@ EventUtil.ContinueOnAddOnLoaded("Blizzard_Professions", function()
 
     hooksecurefunc(ProfessionsFrame.OrdersPage, "SendOrderRequest", function(self)
         resetOrderList()
-    end)
-
-    hooksecurefunc(ProfessionsFrame.OrdersPage, "SetupTable", function(self)
-        if self.orderType == 3 then
-            local PTC = ProfessionsTableConstants;
-            local column = self.tableBuilder:AddFixedWidthColumn(self, PTC.NoPadding, 60, PTC.Tip.LeftCellPadding,
-                PTC.Tip.RightCellPadding, nil, "ProfessionsCrafterTableCellCustomHideTemplate")
-            column:ConstructHeader("BUTTON", "ProfessionsCrafterTableHeaderStringTemplate", self, "Hide")
-            self.tableBuilder:Arrange()
-        end
     end)
 
     hooksecurefunc(ProfessionsFrame.OrdersPage, "ShowGeneric", function(self)
